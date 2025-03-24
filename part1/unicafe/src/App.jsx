fa0bb0040002137625cdbb07d24f1a5567e43b14
crisheiss <cristobal.heiss@joingsg.com>
2025-03-24 14:32:56 +0100

    resolve 1.6 with multiple handlers

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClickGood = () => {
+    return setGood(good + 1)
+  }
+
+  const handleClickNeutral = () => {
+    return setNeutral(neutral + 1)
+  }
+
+  const handleClickBad = () => {
+    return setBad(bad + 1)
+  }
+
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={handleClickGood}>good</button>
+      <button onClick={handleClickNeutral}>neutral</button>
+      <button onClick={handleClickBad}>bad</button>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+export default App
