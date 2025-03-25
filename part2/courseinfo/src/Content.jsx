ef81b30b6332dbba5f8e742fd355bdbd5b61de27
crisheiss <cristobal.heiss@joingsg.com>
2025-03-25 13:31:11 +0100

    finish exercises until 2.5

--- part2/courseinfo/src/Content.jsx
+++ part2/courseinfo/src/Content.jsx
@@ -2,6 +2,6 @@ import { Part } from "./Part";
 
 export const Content = ({parts}) => (
   <div>
-    {parts.map(part => <Part key={part.name} part={part.name} exercises={part.exercises} />)}
+    {parts.map(part => <Part key={part.id} part={part.name} exercises={part.exercises} />)}
   </div>
 );
