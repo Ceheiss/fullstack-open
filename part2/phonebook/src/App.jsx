e2817255e95dcef5d0d1a342fed3f4460c54930a
crisheiss <cristobal.heiss@joingsg.com>
2025-03-26 09:49:19 +0100

    finish exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '99-33-44' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('');
+  const [filteredName, setFilteredName] = useState('');
   const [newNumber, setNewNumber] = useState('');
 
   const handleInputNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleInputFilterChange = (event) => {
+    setFilteredName(event.target.value)
   }
 
   const handleInputNumberChange = (event) => {
@@ -18,7 +26,6 @@ const App = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     const isRepeated = Boolean(persons.find(person => person.name == newName));
-    console.log(isRepeated)
     !isRepeated ?
       setPersons([...persons, { name: newName, number: newNumber }])
     :
@@ -26,10 +33,16 @@ const App = () => {
     setNewName('')
   }
 
+  const filteredPersons = persons.filter(person => person.name.toUpperCase().includes(filteredName.toUpperCase()));
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input value={filteredName} onChange={handleInputFilterChange}/>
+      </div>
       <form onSubmit={handleSubmit}>
+        <h2>add new</h2>
         <div>
           name: <input value={newName} onChange={handleInputNameChange} />
         </div>
@@ -41,7 +54,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => (
+      {filteredPersons.map(person => (
         <div key={person.name}>{person.name} {person.number}</div>
         ))}
     </div>
