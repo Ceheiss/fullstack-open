66054ec552ec85ccfbee174e41e218b1e9504ba8
Ceheiss <cristobal.heiss.j@gmail.com>
2025-04-30 11:35:57 +0200

    exercise 3.2

--- part3/phonebook-api/index.js
+++ part3/phonebook-api/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const port = 3001;
+
 const persons = [
   { 
     "id": "1",
@@ -28,6 +30,11 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 });
 
-app.listen(3001, () => {
-  console.log("App Running")
+app.get('/info', (req, res) => {
+  const date = new Date().toString();
+  res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+});
+
+app.listen(port, () => {
+  console.log(`App Running on port ${port}`)
 })
