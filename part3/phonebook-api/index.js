869cafde8c5cf727c2ba6c862cc6913116c0942c
Ceheiss <cristobal.heiss.j@gmail.com>
2025-04-30 11:40:42 +0200

    exercise 3.3

--- part3/phonebook-api/index.js
+++ part3/phonebook-api/index.js
@@ -35,6 +35,16 @@ app.get('/info', (req, res) => {
   res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
 });
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+  const person = persons.find(person => person.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end()
+  }
+});
+
 app.listen(port, () => {
   console.log(`App Running on port ${port}`)
 })
